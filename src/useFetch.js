dc102376d1d43cbe234075c233050652d0562ae7
dopeMike <mihai.dragomir88@yahoo.com>
2023-07-26 19:03:02 +0300

    new version of application

---
+++ src/useFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCount = new AbortController();
+
+    setTimeout(() => {
+      fetch(url, {signal: abortCount.signal})
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        if(err.name == 'AbortError'){
+          console.log("AbortError")
+        }else{
+          setIsPending(false);
+          setError(err.message);
+        }
+        
+      })
+    }, 1000);
+
+    return () => {
+      abortCount.abort();
+    }
+  }, [url])
+
+  return { data, isPending, error };
+}
+ 
+export default useFetch;
